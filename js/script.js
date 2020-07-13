6d9d42ad89537daedfae320179a4c846d7e63b96
Marco <marco.longo@hotmail.it>
2020-07-13 18:18:13 +0200

    Aggiunto orario reale al messaggio inviato.

--- js/script.js
+++ js/script.js
@@ -13,10 +13,14 @@ $(document).ready(init);
 // Lista funzioni
 function addMessage() {
   $('#send-msg').click(function () {
+
+    var dt = new Date();
+    var time = dt.getHours() + ":" + dt.getMinutes();
+
     var content = $('#content-msg').val();
     var ownerMsg = $('#owner-msg-templ').clone();
     ownerMsg.text(content);
-    ownerMsg.append('<span class="hour-msg">HH:mm</span>');
+    ownerMsg.append('<span class="hour-msg">' + time + '</span>');
     $('.text-space').append(ownerMsg);
     ownerMsg.show();
     $('#content-msg').val('');
