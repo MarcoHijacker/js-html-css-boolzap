aee03bd5cfff8e883c1f840bc89dc3632b9b7f41
Marco <marco.longo@hotmail.it>
2020-07-14 08:47:09 +0200

    Aggiunta risposta auto dopo 1 sec.

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@
 function init() {
 
   addMessage();
+  addMessageByKey();
 
 }
 
@@ -24,5 +25,37 @@ function addMessage() {
     $('.text-space').append(ownerMsg);
     ownerMsg.show();
     $('#content-msg').val('');
+    setTimeout(autoReply, 1000);
   });
+}
+
+function addMessageByKey() {
+  $(document).keydown(function () {
+  var key;
+  key = event.which;
+  if (key == 13) {
+    var dt = new Date();
+    var time = dt.getHours() + ":" + dt.getMinutes();
+
+    var content = $('#content-msg').val();
+    var ownerMsg = $('#owner-msg-templ').clone();
+    ownerMsg.text(content);
+    ownerMsg.append('<span class="hour-msg">' + time + '</span>');
+    $('.text-space').append(ownerMsg);
+    ownerMsg.show();
+    $('#content-msg').val('');
+    setTimeout(autoReply, 1000);
+  }
+});
+}
+
+function autoReply() {
+  var dt = new Date();
+  var time = dt.getHours() + ":" + dt.getMinutes();
+
+  var contactMsg = $('#contact-msg-templ').clone();
+  contactMsg.text('Ok!');
+  contactMsg.append('<span class="hour-msg">' + time + '</span>');
+  $('.text-space').append(contactMsg);
+  contactMsg.show();
 }
