746f0d1c9124473aecf4984a7fd3333f26fe859c
Marco <marco.longo@hotmail.it>
2020-07-14 20:18:44 +0200

    Aggiunta ricerca conversazioni.

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ function init() {
   showArrow();
   showMenu();
   deleteMsg();
+  liveSearch();
 
 }
 
@@ -37,7 +38,7 @@ function addMessage() {
 }
 // Funzione di aggiunta messaggio con il tasto invio
 function addMessageByKey() {
-  $(document).keydown(function () {
+  $('#content-msg').keydown(function () {
   var key = event.which;
   var content = $('#content-msg').val();
 
@@ -59,8 +60,8 @@ function addMessageByKey() {
 function autoReply() {
   var dt = new Date();
   var time = dt.getHours() + ":" + dt.getMinutes();
-
   var contactMsg = $('#contact-msg-templ').clone();
+
   contactMsg.find('#contact-msg-cont').text('Ok!');
   contactMsg.find('#contact-msg-hour').text(time);
   $('.text-space').append(contactMsg);
@@ -97,5 +98,23 @@ function deleteMsg() {
   });
   $(document).on('click', 'div.contact-msg .dropdown-delete', function() {
     $(this).parent().parent().remove();
+  });
+}
+// Funzione per cercare conversazioni
+// Non l'ho fatta io, l'ho solo adattata al mio html. Recuperata da: https://www.w3schools.com/jquery/jquery_filters.asp
+function liveSearch() {
+  var convArray = $('.contact-area .contact-conv .field-info .field-name');
+
+  $('#search-conv').keydown(function() {
+    var key = event.which;
+
+    if(key == 13) {
+      $("#search-conv").on("keyup", function() {
+        var value = $(this).val().toLowerCase();
+        $(".contact-area .field-name").filter(function() {
+          $(this).parent().parent().toggle($(this).text().toLowerCase().indexOf(value) > -1)
+        });
+      });
+    }
   });
 }
